1edf914edc82a025d92b5d4a93e8559dde914ec1
George Thorne <your_email@example.com>
2024-08-21 21:18:20 +0300

    ded

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,7 +125,7 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.home__title, .popular__container, .subscribe__container, .footer__container`)
-sr.reveal(`.home__description, .footer__info`, {delay: 500})
+sr.reveal(`.home__description, .footer__info`, {delay: 200})
 sr.reveal(`.home__search`, {delay: 600})
 sr.reveal(`.home__value`, {delay: 700})
 sr.reveal(`.home__images`, {delay: 800, origin: 'bottom'})
